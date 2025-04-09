6f5da79a34e22b338de01fed8b903940af2caba3
GraceRuan <grace.ruan@gov.bc.ca>
2025-04-08 21:51:53 -0700

    fix: format ts files

--- workflow-cli/src/services/opensearch-rollup.service.ts
+++ workflow-cli/src/services/opensearch-rollup.service.ts
@@ -1,11 +1,12 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import * as fs from 'fs';
 import * as path from 'path';
 import AwsService from './aws.service';
 import { WorkflowSettings } from './opensearch-domain.service';
 
 export default class OpenSearchRollupService extends AwsService {
-  public async createOrUpdateRollupJob(settings: WorkflowSettings): Promise<any> {
+  public async createOrUpdateRollupJob(
+    settings: WorkflowSettings,
+  ): Promise<any> {
     const templateDir = path.resolve(
       __dirname,
       '../../configuration-opensearch/rollup_index_jobs',
@@ -15,25 +16,38 @@ export default class OpenSearchRollupService extends AwsService {
       if (!filePath.endsWith('.json')) {
         continue;
       }
-      const rollupJobName = path.basename(filePath, '.json');    
-      let rollupJobPath = `/_plugins/_rollup/jobs/${rollupJobName}`;    
+      const rollupJobName = path.basename(filePath, '.json');
+      let rollupJobPath = `/_plugins/_rollup/jobs/${rollupJobName}`;
       const exists = await this.checkRollupJobExists(settings, rollupJobName);
-      const rollupConfig = JSON.parse(fs.readFileSync(path.join(templateDir, filePath), 'utf8'));
+      const rollupConfig = JSON.parse(
+        fs.readFileSync(path.join(templateDir, filePath), 'utf8'),
+      );
 
       if (exists) {
         rollupJobPath += '?if_seq_no=1&if_primary_term=1';
         console.log(`Updating existing rollup job: ${rollupJobName}`);
-        return this.executeRollupRequest('PUT', settings, rollupJobPath, rollupConfig)
-        .then((res) => {
-          console.log(`PUT ${rollupJobPath}\n[${res.statusCode}] Rollup job updated`);
+        return this.executeRollupRequest(
+          'PUT',
+          settings,
+          rollupJobPath,
+          rollupConfig,
+        ).then((res) => {
+          console.log(
+            `PUT ${rollupJobPath}\n[${res.statusCode}] Rollup job updated`,
+          );
         });
-      }
-      else {
+      } else {
         console.log(`Creating new rollup job: ${rollupJobName}`);
-        return this.executeRollupRequest('PUT', settings, rollupJobPath, rollupConfig)
-          .then((res) => {
-            console.log(`PUT ${rollupJobPath}\n[${res.statusCode}] Rollup job created`);
-          });
+        return this.executeRollupRequest(
+          'PUT',
+          settings,
+          rollupJobPath,
+          rollupConfig,
+        ).then((res) => {
+          console.log(
+            `PUT ${rollupJobPath}\n[${res.statusCode}] Rollup job created`,
+          );
+        });
       }
     }
   }
@@ -42,13 +56,12 @@ export default class OpenSearchRollupService extends AwsService {
     method: string,
     settings: WorkflowSettings,
     path: string,
-    body?: any
+    body?: any,
   ): Promise<any> {
-
     const [pathWithoutQuery, queryString] = path.split('?');
     const queryParams = queryString
-    ? Object.fromEntries(new URLSearchParams(queryString).entries())
-    : undefined;
+      ? Object.fromEntries(new URLSearchParams(queryString).entries())
+      : undefined;
     const requestOptions = {
       method,
       headers: {
@@ -56,21 +69,26 @@ export default class OpenSearchRollupService extends AwsService {
         host: settings.hostname,
       },
       hostname: settings.hostname,
-      path:pathWithoutQuery,
+      path: pathWithoutQuery,
       query: queryParams,
       body: body ? JSON.stringify(body) : undefined,
     };
-  
+
     return this.executeSignedHttpRequest(requestOptions)
       .then((res) => this.waitAndReturnResponseBody(res))
       .catch((err) => {
-        console.error(`Error during ${method} request to ${path}: ${err.message}`);
+        console.error(
+          `Error during ${method} request to ${path}: ${err.message}`,
+        );
         throw err;
       });
   }
 
   // check if the rollup job exists
-  private async checkRollupJobExists(settings: WorkflowSettings, jobName: string): Promise<boolean> {
+  private async checkRollupJobExists(
+    settings: WorkflowSettings,
+    jobName: string,
+  ): Promise<boolean> {
     const rollupJobPath = `/_plugins/_rollup/jobs/${jobName}`;
     return this.executeRollupRequest('GET', settings, rollupJobPath)
       .then((res) => {
@@ -93,5 +111,4 @@ export default class OpenSearchRollupService extends AwsService {
         throw err;
       });
   }
-
 }
