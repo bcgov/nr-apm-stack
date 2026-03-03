c8a92d961c0c1e255030fcdc66db92ef96c16f29
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2026-03-03 14:54:48 -0800

    fix: update cron setup

--- iam-key-sync/src/app/iam-key-sync-cron.sh
+++ iam-key-sync/src/app/iam-key-sync-cron.sh
@@ -2,11 +2,24 @@
 
 
 env $(cat $GITHUB_ENV | xargs) ./intention-open.sh
+if [ $? -ne 0 ]; then
+  echo "Error: intention-open.sh failed"
+  exit 1
+fi
+
 source $GITHUB_ENV
 env $(cat $GITHUB_ENV | xargs) ACTION_TOKEN=$ACTION_TOKEN_SYNC ./action-start.sh
 env $(cat $GITHUB_ENV | xargs) ACTION_TOKEN=$ACTION_TOKEN_SYNC ./vault-login.sh
 
 env $(cat $GITHUB_ENV | xargs) ACTION_TOKEN=$ACTION_TOKEN_SYNC ./iam-key-sync-runner.sh
+
+if [ $? -ne 0 ]; then
+  echo "Error: iam-key-sync-runner.sh failed"
+  env $(cat $GITHUB_ENV | xargs) ACTION_TOKEN=$ACTION_TOKEN_SYNC OUTCOME=failure ./action-end.sh
+  env $(cat $GITHUB_ENV | xargs) ./vault-token-revoke.sh
+  env $(cat $GITHUB_ENV | xargs) OUTCOME=failure ./intention-close.sh
+  exit 1
+fi
 
 env $(cat $GITHUB_ENV | xargs) ACTION_TOKEN=$ACTION_TOKEN_SYNC OUTCOME=success ./action-end.sh
 env $(cat $GITHUB_ENV | xargs) ./vault-token-revoke.sh
